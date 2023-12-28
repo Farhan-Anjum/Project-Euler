dca1dee680f5c92bbda3dcc236bacf1cef885ddd
Farhan Anjum <your.email@example.com>
2023-12-28 23:10:05 +0500

    Multiple of 3 and 5 and Largest Prime Factor of number: 600851475143

--- Problem_No.1/Problem_1.js
+++ Problem_No.1/Problem_1.js
@@ -2,7 +2,7 @@ const sumOfMultiples  = function (limit){
     let sum = 0;
     for (i = 1; i<=limit; i++){
         if(i % 3 == 0 || i % 5==0){
-            sum += i; 
+            sum += i; //sum =sum +i;
         }
     }
 return sum;
